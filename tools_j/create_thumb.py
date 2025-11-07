2ebd952184bff9ecfea3094a80b13eb79b5a15ee
jenny <jenny.finsrud@gmail.com>
2025-11-07 12:42:15 +0100

    automatically creates directory

--- tools_j/create_thumb.py
+++ tools_j/create_thumb.py
@@ -1,24 +1,24 @@
 import pyvips
 from pathlib import Path
 
-def create_thumbnail(input_tiff_path : str | Path, output_thumbnail_path : str | Path):
+def create_thumbnail(input_tiff_path : str | Path, output_thumbnail_path : str | Path, thumb_name: str):
     # Load the TIFF image
     image = pyvips.Image.new_from_file(input_tiff_path, access='sequential')
 
-    # Calculate the scale needed to resize the image
+    # How much image is scaled
     scale = 0.2
     # Resize the image to the thumbnail size
     thumbnail = image.resize(scale)
 
     # Save thumbnail
-    thumbnail.write_to_file(output_thumbnail_path)
+    thumbnail.write_to_file(output_thumbnail_path / thumb_name)
 
 if __name__ == "__main__":
     # Input and output path
-    input_tiff_path = "/media/jenny/Expansion/MM_HE_results/HE_MM179_2D_290125_20x_BF_01/mask/wsi/whole_image_complete.png"
-    # input_tiff_path = "/media/jenny/Expansion/aSMA/Image_MM179_A_SMA.tif"
-    # input_tiff_path = "/media/jenny/Expansion/MM_HE_20x_TIFF/HE_MM179_A_70225_vsi_Collection/HE_MM179_A_70225_20x_BF_01/HE_MM179_A_70225_20x_BF_01.tif"
-    output_thumbnail_path = "/media/jenny/Expansion/MM_HE_thumbnail/MM179_2D/result_HE_MM179_2D_290125_20x_BF_01.png"
-    
-
-    create_thumbnail(input_tiff_path, output_thumbnail_path)
+    input_tiff_path = "/media/.../image.tif"
+    output_thumbnail_path = Path("/media/.../")
+    thumb_name = "x_scaled.png"
+    if not output_thumbnail_path.exists():
+        output_thumbnail_path.mkdir(parents=True) 
+        print(f"Directory {output_thumbnail_path} was created")
+    create_thumbnail(input_tiff_path, output_thumbnail_path, thumb_name)
